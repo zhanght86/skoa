650a2e57677b5e993ad7e368ba638dcd4d337cf1
evan <81367070@qq.com>
2016-11-11 10:27:44 +0800

    restful api；add timestamp;add complex appKey

--- jeesite-oa/src/main/java/com/thinkgem/jeesite/restful/codec/HmacSHA256Utils.java
+++ jeesite-oa/src/main/java/com/thinkgem/jeesite/restful/codec/HmacSHA256Utils.java
@@ -77,7 +77,7 @@ public class HmacSHA256Utils {
             }
 
         }
-        System.out.println(s.toString());
+        //System.out.println(s.toString());
         return digest(key, s.toString());
     }
 
