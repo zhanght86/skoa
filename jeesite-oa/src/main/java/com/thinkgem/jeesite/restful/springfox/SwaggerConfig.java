9390af58cba7b7f4b180a0376cba40e1242d5b1b
evan <81367070@qq.com>
2016-11-12 09:29:23 +0800

    add springfox swagger

--- jeesite-oa/src/main/java/com/thinkgem/jeesite/restful/springfox/SwaggerConfig.java
+++ jeesite-oa/src/main/java/com/thinkgem/jeesite/restful/springfox/SwaggerConfig.java
@@ -20,13 +20,15 @@ public class SwaggerConfig {
 	@Bean
 	public Docket statelessRestfullApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("ios")
+				.groupName("all")
 				.genericModelSubstitutes(DeferredResult.class)
 				//.genericModelSubstitutes(ResponseEntity.class)
 				.useDefaultResponseMessages(false)
 				.forCodeGeneration(true)
-				.pathMapping("/api")//api测试请求地址
+				//.pathMapping("/api")//api测试请求地址
+				.pathMapping("/")//api测试请求地址
 				.select()
+				//.paths(PathSelectors.any())//过滤的接口
 				.paths(PathSelectors.regex("/api/.*"))//过滤的接口
 				.build()
 				.apiInfo(statelessRestfullApiInfo());
@@ -34,10 +36,15 @@ public class SwaggerConfig {
 
 	private ApiInfo statelessRestfullApiInfo() {
 		Contact contact = new Contact("evan", "https://swmwlm.github.io", "81367070@qq.com");
+		StringBuilder sb=new StringBuilder();
+		sb.append("1.客户端需要申请appId和appKey").append("<br />");
+		sb.append("2.算法步骤a):按nonce+appKey+timestamp字符串拼装,使用该字符串对参数map集合产生的待加密字符串进行HmacSHA256信息摘要,防止数据被篡改,并防止重放攻击.").append("<br />");
+		sb.append("3.算法步骤b):参数map集合由appId,timestamp,nonce固定参数以及待调用api方法中的参数组成,待加密的字符串是通过遍历map集合key,以及value拼装得到.").append("<br />");
+		sb.append("   待加密字符串形如:appId:appIdStrcontent:内容是什么nonce:1111param1:1param12param11param2:param2projectName:项目标题timestamp:1478825956").append("<br />");
 		ApiInfo apiInfo = new ApiInfo("首科OA平台restful api",//大标题
-				"stateless restful api",//小标题
+				sb.toString(),
 				"v1",//版本
-				"https://shoukeplus.com",
+				"https://oa.shoukeplus.com",
 				contact,//作者
 				"Apache Licence 2.0",//licenses链接显示文字
 				"http://www.apache.org/licenses/"//licenses链接
---
+++ jeesite-oa/src/main/java/com/thinkgem/jeesite/restful/web/api/v1/UserControllerApi.java
@@ -0,0 +1,86 @@
+package com.thinkgem.jeesite.restful.web.api.v1;
+
+import com.google.common.base.Preconditions;
+import com.thinkgem.jeesite.common.json.JsonResultModel;
+import com.thinkgem.jeesite.modules.project.entity.ProjectInfo;
+import com.thinkgem.jeesite.modules.sys.entity.User;
+import com.thinkgem.jeesite.restful.web.api.BaseController;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/api",headers="api-version=v1")
+@Api(value = "User控制器")
+public class UserControllerApi extends BaseController {
+
+    // @ApiIgnore //使用这个注解忽略这个接口
+    @ApiOperation(value = "根据ProjectInfo projectInfo, String[] param1, String param2进行hello方法调用", httpMethod = "POST", produces = "application/json",consumes = "application/x-www-form-urlencoded")
+    @ApiResponse(code = 200, message = "success", response = JsonResultModel.class)
+    @ApiImplicitParam(name = "projectInfo", value = "项目实体projectInfo", required = true, dataType = "ProjectInfo",paramType="query")
+    @RequestMapping(value = "/hello",method = RequestMethod.POST)
+    public JsonResultModel hello(ProjectInfo projectInfo, @ApiParam(name = "param1", value = "字符数组", required = true) String[] param1, @ApiParam(name = "param2", value = "字符串", required = true) String param2) {
+        try {
+            Preconditions.checkNotNull(param1,"参数param1不能为空");
+            Preconditions.checkNotNull(param2,"参数param2不能为空");
+            jsonResultModel.setStateSuccess();
+            jsonResultModel.setData(new User("1","evan"));
+            System.out.println(projectInfo);
+        }catch (Exception e){
+            e.printStackTrace();
+            logger.error("参数校验失败：", e);
+            jsonResultModel.setMessage(e.getMessage());
+            jsonResultModel.setStateError();
+        }
+        return jsonResultModel;
+    }
+
+    /*-----------------------------------restful api demo----------------------------------------------*/
+
+    static Map<String, User> users = Collections.synchronizedMap(new HashMap<String, User>());
+
+    @ApiOperation(value="获取用户列表", notes="")
+    @RequestMapping(value={"/users"}, method=RequestMethod.GET)
+    public List<User> getUserList() {
+        List<User> r = new ArrayList<User>(users.values());
+        return r;
+    }
+
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    @RequestMapping(value="/users", method=RequestMethod.POST)
+    public String postUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "String")
+    @RequestMapping(value="/users/{id}", method=RequestMethod.GET)
+    public User getUser(@PathVariable String id) {
+        return users.get(id);
+    }
+
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
+    @RequestMapping(value="/users/{id}", method=RequestMethod.PUT)
+    public String putUser(@PathVariable String id, @RequestBody User user) {
+        User u = users.get(id);
+        u.setName(user.getName());
+        u.setEmail(user.getEmail());
+        users.put(id, u);
+        return "success";
+    }
+
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "String")
+    @RequestMapping(value="/users/{id}", method=RequestMethod.DELETE)
+    public String deleteUser(@PathVariable String id) {
+        users.remove(id);
+        return "success";
+    }
+}
